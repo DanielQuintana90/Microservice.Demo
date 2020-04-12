5f3ec16e64081e73618e8289670ff2edfdaaeebf
DanielQuintana <danielquintana90@gmail.com>
2020-04-11 22:28:29 -0500

    Configured Swagger

---
+++ Microservices.Demo.Report.API/Controllers/ReportController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Microservices.Demo.Report.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ReportController : ControllerBase
+    {
+        [HttpGet]
+        [Route("GetPolicies")]
+        public async Task<ActionResult> GetPolicies()
+        {
+            var json = new { status = "Funciona el Swagger" };
+
+            return new JsonResult(json);
+        }
+    }
+}
